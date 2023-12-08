d26906a92aa128a33a6afda25a5c064f4563b694
Mikel López <mlvillarroya@gmail.com>
2023-12-08 23:04:34 +0100

    day08b!!!

--- days/day08b.py
+++ days/day08b.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 def loadData():
     with open(".\\days\\08_input.txt", "r") as f:
@@ -16,20 +17,34 @@ def loadData():
 def searchForInitialNodes(myMaps):
     return [node for node in myMaps.keys() if node.endswith("A")]
 
-def allNodesAreFinal(currentStops):
-    return all(node.endswith("Z") for node in currentStops)
+# def allNodesAreFinal(currentStops):
+#     return all(node.endswith("Z") for node in currentStops)
 
 def execute():
     guidelines, myMap = loadData()
     currentStops = searchForInitialNodes(myMap)
     steps = 0
-    while not allNodesAreFinal(currentStops):
-        for currentChar in guidelines:
-            for i in range(len(currentStops)):
+    # while not allNodesAreFinal(currentStops):
+    #     for currentChar in guidelines:
+    #         for i in range(len(currentStops)):
+    #             if currentChar == 'L': 
+    #                 currentStops[i] = myMap[currentStops[i]][0]
+    #             else: 
+    #                 currentStops[i] = myMap[currentStops[i]][1]
+    #         steps += 1
+    #         if allNodesAreFinal(currentStops): break
+    # print(steps)
+    globalSteps = []
+    for i in range(len(currentStops)):
+        currentStop = currentStops[i]
+        steps = 0
+        while not currentStop.endswith('Z'):
+            for currentChar in guidelines:
                 if currentChar == 'L': 
-                    currentStops[i] = myMap[currentStops[i]][0]
+                    currentStop = myMap[currentStop][0]
                 else: 
-                    currentStops[i] = myMap[currentStops[i]][1]
-            steps += 1
-            if allNodesAreFinal(currentStops): break
-    print(steps)
+                    currentStop = myMap[currentStop][1]
+                steps += 1
+                if currentStop.endswith('Z'): break
+        globalSteps.append(steps)
+    print(math.lcm(*globalSteps))
